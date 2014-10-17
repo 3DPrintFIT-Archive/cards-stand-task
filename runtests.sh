985d33b12d0c2aef53c5c5edf0f1291cd816af4e
Miro Hrončok <miro@hroncok.cz>
2014-10-18 00:24:38 +0200

    Basic HTML reports

--- runtests.sh
+++ runtests.sh
@@ -7,13 +7,21 @@ mkdir good 2>/dev/null
 rm good/*.png 2>/dev/null
 
 tests() {
+  REPORT=index.html
+  echo '<html><body>' > $REPORT
   for i in $(seq 1 $MAXTEST); do
     openscad -D t=$i -o t$i.png tests.scad --render --camera=0,0,0,55,0,25,140 --imgsize=400,400
+    echo "<h1>$i</h1>" >> $REPORT
+    echo "<img src=\"../../good/t$i.png\" title=\"good\"/>" >> $REPORT
+    echo "<img src=\"t$i.png\" title=\"student\"/>" >> $REPORT
+    echo "<hr />" >> $REPORT
   done
+  echo '</body></html>' >> $REPORT
 }
 
 tests
 mv *.png good/
+rm index.html
 
 for DIR in students/*; do
   pushd $DIR > /dev/null
