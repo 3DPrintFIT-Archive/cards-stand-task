81a3f63e5d51ee785f4d40c86edf9f12f6d1cfdc
Miro Hrončok <miro@hroncok.cz>
2014-10-21 10:59:31 +0200

    Better script output (more quiet and more informative)

--- runtests.sh
+++ runtests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-MAXTEST=`grep "if (t == " tests.scad | wc -l`
+MAXTEST=`grep "^if (t == " tests.scad | wc -l`
 
 mkdir students 2>/dev/null
 mkdir good 2>/dev/null
@@ -9,21 +9,26 @@ rm good/*.png 2>/dev/null
 tests() {
   REPORT=index.html
   echo '<html><body>' > $REPORT
+  rm t*.png 2>/dev/null
   for i in $(seq 1 $MAXTEST); do
-    openscad -D t=$i -o t$i.png tests.scad --render --camera=0,0,0,55,0,25,140 --imgsize=400,400
+    openscad -D t=$i -o t$i.png tests.scad --render --camera=0,0,0,55,0,25,140 --imgsize=400,400 2>/dev/null
+    echo -n .
     echo "<h1>$i</h1>" >> $REPORT
     echo "<img src=\"../../good/t$i.png\" title=\"good\"/>" >> $REPORT
     echo "<img src=\"t$i.png\" title=\"student\"/>" >> $REPORT
     echo "<hr />" >> $REPORT
   done
+  echo
   echo '</body></html>' >> $REPORT
 }
 
+echo -n good
 tests
 mv *.png good/
 rm index.html
 
 for DIR in students/*; do
+  echo -n ${DIR#*/}
   pushd $DIR > /dev/null
   cp ../../tests.scad .
   tests
