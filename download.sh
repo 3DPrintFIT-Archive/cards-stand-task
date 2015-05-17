c377b3c40787b5cf09b854091a7bee8bed3fb0bb
Miro Hrončok <miro@hroncok.cz>
2015-05-17 23:53:01 +0200

    Only test second unizp if the first failed

--- download.sh
+++ download.sh
@@ -24,11 +24,11 @@ for USER in $USERNAMES; do
     rm box.zip
     curl -O https://edux.fit.cvut.cz/courses/BI-3DT/_media/en/student/$USER/box.zip?purge --cookie "$COOKIE"
     mv -f 'box.zip?purge' box.zip
-  fi
-  if ! unzip -o box.zip; then
-    popd > /dev/null
-    rm -rf $dir
-    continue
+    if ! unzip -o box.zip; then
+      popd > /dev/null
+      rm -rf $dir
+      continue
+    fi
   fi
   rm box.zip
   popd > /dev/null
